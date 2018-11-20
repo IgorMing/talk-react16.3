0037300ad3c96d8f63c1f99ef0eaac1a3a8c575b
Igor Ming <igor.ming@gmail.com>
2018-11-20 01:44:44 -0200

    do index.js reference app.js correctly

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Context from './Context';
+// import Context from './Context';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Context />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
