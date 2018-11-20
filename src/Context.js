354a1a1fb7f41d465be2fb740c225ba71866e21d
Igor Ming <igor.ming@gmail.com>
2018-11-20 01:22:31 -0200

    small change in context component

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Parent extends Component {
+  state = {
+    message: 'Hi people!'
+  }
+
   render() {
     return (
-      <FirstChild msg="Hi people!" />
+
+      <FirstChild msg={this.state.message} />
     );
   }
 }
